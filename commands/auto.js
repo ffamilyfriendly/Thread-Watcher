414690fabbfce3acefad66dfbf5c8a194e41a761
PlavorSeol <pseol2190@outlook.com>
2021-12-18 21:31:58 +0900

    Make permission error response ephemeral
    Oops, mistake

--- commands/auto.js
+++ commands/auto.js
@@ -6,7 +6,7 @@ const run = (client, data, respond) => {
 
   // Manage Channels or Manage Server
   if (permissions & (1 << 4) === (1 << 4) || permissions & (1 << 5) === (1 << 5)) {
-    respond('You do not have permission to use /auto command.', 'You need Manage Channels or Manage Server permission to use it.', '#ff0000', false);
+    respond('You do not have permission to use /auto command.', 'You need Manage Channels or Manage Server permission to use it.', '#ff0000', true);
   }
 
   const thread = data.data.resolved.channels[Object.keys(data.data.resolved.channels)[0]];
