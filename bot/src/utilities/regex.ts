393ccd755b79c2b40715c78bbc57ddeba4660e72
null <60427892+null8626@users.noreply.github.com>
2024-03-28 21:37:41 +0700

    refactor[regex]: le refactor

--- bot/src/utilities/regex.ts
+++ bot/src/utilities/regex.ts
@@ -2,26 +2,19 @@
 const quantifier = "\\p{L}\\p{Emoji_Presentation}><\\(\\)!*\\-\\[\\]\\d"
 
 export function validRegex(r: string) {
-    if(r.trim() === '') return { valid: false, reason: "regex string empty" }
-    if(!/^[\p{L}\p{Emoji_Presentation}><\(\)!*\-\[\]\d]{0,100}$/gmu.test(r)) return { valid: false, reason: "invalid regex format" }
+    if (!r.trim().length) return { valid: false, reason: "regex string empty" }
+    if (!/^[\p{L}\p{Emoji_Presentation}><\(\)!*\-\[\]\d]{0,100}$/gmu.test(r)) return { valid: false, reason: "invalid regex format" }
 
     return { valid: true }
 }
 
 export function strToRegex(r: string) {
     let inverted = false
-    if(r[0] === '!') {
+    if (r[0] === '!') {
         inverted = true
-        r = r.replace('!','')
+        r = r.replace('!', '')
     }
     
-    r = r.replace(/(\]|\[|\)|\()/gi, "\\$1")
-    r = r.replace(/\*+/gi, "*")
-
-    r = "^" + r + "$"
-
-
-    r = r.replaceAll('*', `[${quantifier}]*`)
-
-    return { inverted, regex: new RegExp(r, "gmu")}
+    r = r.replace(/(\]|\[|\)|\()/g, "\\$1").replace(/\*{1,}/g, `[${quantifier}]*`)
+    return { inverted, regex: new RegExp(`^${r}$`, "gmu")}
 }
