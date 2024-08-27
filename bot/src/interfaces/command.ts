f2a3b20323c0c4fb3de8869353ea4a0028494959
ffamilyfriendly <me@familyfriendly.xyz>
2024-08-27 03:03:05 +0200

    fixes: issues that arose after updating d.js ver

--- bot/src/interfaces/command.ts
+++ bot/src/interfaces/command.ts
@@ -1,31 +1,53 @@
-import { ColorResolvable, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder, PermissionResolvable, ChatInputCommandInteraction, EmbedBuilder, AutocompleteInteraction, ActionRowBuilder, MessageActionRowComponentData, MessageActionRowComponentBuilder, APIMessageActionRowComponent, Interaction, SelectMenuInteraction } from "discord.js";
+import {
+  ColorResolvable,
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+  PermissionResolvable,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  AutocompleteInteraction,
+  ActionRowBuilder,
+  SlashCommandOptionsOnlyBuilder,
+} from "discord.js";
 
 export enum statusType {
-    error = "error",
-    success = "success",
-    info = "info",
-    warning = "warning"
+  error = "error",
+  success = "success",
+  info = "info",
+  warning = "warning",
 }
 
-export type baseEmbedOptions = {
-    description?: string,
-    color?: ColorResolvable,
-    fields?: { name: string; value: string }[],
-    ephermal?: Boolean,
-    showAuthor?: Boolean,
-    components?: ActionRowBuilder<any>[],
-    noSend?: boolean
+export interface baseEmbedOptions {
+  description?: string;
+  color?: ColorResolvable;
+  fields?: { name: string; value: string }[];
+  ephermal?: boolean;
+  showAuthor?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  components?: ActionRowBuilder<any>[];
+  noSend?: boolean;
 }
 
 export interface Command {
-    data: Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand"> | SlashCommandSubcommandsOnlyBuilder,
-    gatekeeping?: {
-        ownerOnly: Boolean,
-        userPermissions?: PermissionResolvable[],
-        botPermissions?: PermissionResolvable[]
-        devServerOnly: Boolean
-    },
-    externalOptions?: any[],
-    run: (interaction: ChatInputCommandInteraction, buildBaseEmbed: (title: String, status: statusType, misc?: baseEmbedOptions) => EmbedBuilder ) => Promise<void>,
-    autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>
+  data:
+    | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashCommandOptionsOnlyBuilder;
+  gatekeeping?: {
+    ownerOnly: boolean;
+    userPermissions?: PermissionResolvable[];
+    botPermissions?: PermissionResolvable[];
+    devServerOnly: boolean;
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  externalOptions?: any[];
+  run: (
+    interaction: ChatInputCommandInteraction,
+    buildBaseEmbed: (
+      title: string,
+      status: statusType,
+      misc?: baseEmbedOptions,
+    ) => EmbedBuilder,
+  ) => Promise<void>;
+  autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
 }
