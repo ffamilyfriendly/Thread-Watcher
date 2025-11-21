75c26fb8a3cc9b3e0b40cbdf54dfc614fd77f2ef
Jonathan Hermin <me@familyfriendly.xyz>
2025-11-22 00:55:39 +0100

    adds: manual archival of thread also unwatches it

--- bot/src/events/thread_events/threadUpdate.ts
+++ bot/src/events/thread_events/threadUpdate.ts
@@ -1,8 +1,9 @@
-import { ThreadChannel } from 'discord.js';
+import { AuditLogEvent, ThreadChannel } from 'discord.js';
 import { channel_service, client, logger, thread_service } from 'bot';
 import { Event } from 'interfaces/ClientEvent';
 import ThreadService from 'services/ThreadService';
 import { Logger } from 'tslog';
+import { ResultAsync } from 'neverthrow';
 
 async function check_should_be_watched(thread: ThreadChannel, l: Logger<unknown>) {
   if (!thread.parentId) return;
@@ -38,10 +39,43 @@ async function check_watched_and_bump(thread: ThreadChannel, l: Logger<unknown>)
   if (res_bump.isErr()) return l.error(res_bump.error);
 }
 
-const event: Event<ThreadChannel> = {
+const event: Event<ThreadChannel, ThreadChannel> = {
   event_name: 'threadUpdate',
-  async event_callback(thread) {
+  async event_callback(_old_thread, thread) {
     const l = logger.getSubLogger({ name: 'THREAD_UPDATE' });
+
+    const audit = await ResultAsync.fromPromise(
+      thread.guild.fetchAuditLogs({
+        type: AuditLogEvent.ThreadUpdate,
+        limit: 10,
+      }),
+      (err) => err,
+    ).match(
+      (ok_val) => ok_val,
+      (err_val) => {
+        l.error(`Could not fetch audit log`, err_val);
+        return null;
+      },
+    );
+
+    const audit_entry = audit?.entries.find((audit_entry) => audit_entry.targetId == thread.id);
+    const change = audit_entry?.changes.find((change) => change.key == 'archived');
+
+    if (change?.new) {
+      l.info(
+        `Thread ${thread.id} was unwatched due to manual archival by ${audit_entry?.executorId}`,
+      );
+      thread_service.unwatch_thread(thread);
+
+      return;
+    }
+
+    if (thread.archived && thread.unarchivable) {
+      const could_archive = await ResultAsync.fromPromise(thread.setArchived(false), (err) => err);
+      if (could_archive.isErr()) {
+        l.error(`Could not unarchive ${thread.id}:`, could_archive.error);
+      }
+    }
 
     check_should_be_watched(thread, l);
     check_watched_and_bump(thread, l);
--- bot/src/interfaces/ClientEvent.ts
+++ bot/src/interfaces/ClientEvent.ts
@@ -1,4 +1,4 @@
-export interface Event<T = unknown> {
+export interface Event<T1 = unknown, T2 = unknown> {
   event_name: string;
-  event_callback: (data: T) => void;
+  event_callback: (d1: T1, d2: T2) => void;
 }
